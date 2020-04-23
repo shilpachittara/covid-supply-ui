0e9c0a1712db324cb7a0f80919b60919bb4286fe
shilpa chittara <shilpachittara@shilpas-MacBook-Pro.local>
2020-04-23 10:42:43 +0530

    sitemap edited

--- src/sitemap-builder.js
+++ src/sitemap-builder.js
@@ -21,11 +21,11 @@ const paramsConfig = {
 	'/request-form/:category/:product': [
 		{ category: 'Mask' , product: ['3-ply-Mask', 'N-95']},
 		{ category: 'Coverall' , product: ['Coverall-SS', 'Coverall-SMS', 'Coverall-SSMMS']},
-		{ category: 'Hand-Sanitizer' , product: ['60 percent Alcohol', '70 percent Alcohol', '80 percent Alcohol']},
+		{ category: 'Hand-Sanitizer' , product: ['60-percent-Alcohol', '70-percent-Alcohol', '80-percent-Alcohol']},
 		{ category: 'PPE-kit' , product: ['PPE-SS', 'PPE-SMS', 'PPE-SSMMS']},
 		{ category: 'Gloves'  , product: ['Nitrile']},
 		{ category: 'Bouffant-Caps' , product:['Bouffant-Cap']},
-		{ category: 'Eye-Goggles'  , product:['Eye Goggle']},
+		{ category: 'Eye-Goggles'  , product:['Eye-Goggle']},
 		{ category: 'Thermometer'  , product:['Infrared-Thermometer']},
 		{ category: 'Shoe-Cover'  , product:['Shoe-Cover']},
 		{ category: 'Testing-kit'  , product:['Testing-kit']}
