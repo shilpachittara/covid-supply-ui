05421d5756f45dbe88d2e96fdfdd3ecb857e741c
Anurag Shanu <anurag@trisysit.com>
2020-05-06 12:58:49 +0530

    package.json and dashboard added

--- src/views/examples/Dashboard.js
+++ src/views/examples/Dashboard.js
@@ -14,7 +14,7 @@ import {
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import { Link } from "react-router-dom";
 import DemoFooter from "components/Footers/DemoFooter.js";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import RupeeIcon from 'assets/icons/business.svg'
@@ -36,134 +36,134 @@ function ProfilePage() {
         link={[{ href: "http://covidsupply.in/about-us", rel: "canonical" }]}
         meta={[
           { "name": "description", "content": "Buy certified PPE kits, N95 masks, Nitrile gloves, Face shields in Delhi, Mumbai, Bangalore, Indore, Hyderabad at best prices." },
-          { "name": "keywords", "content": "Covid PPE-KIT Mask Coverall"},
-          { "name": "twitter:card", "content": "summary_large_image"},
-          { "name": "twitter:site", "content": "@supplycovid"},
-          { "name": "twitter:creator", "content": "@supplycovid"},
-          { "name": "twitter_title", "content": "Buy Original PPE kits, n95 masks, nitrile gloves from SITRA certified Manufacturers | Covid Supply"},
-          { "name": "twitter_description", "content": "Buy Original PPE kits, n95 masks, nitrile gloves from SITRA certified Manufacturers | Covid Supply"},
+          { "name": "keywords", "content": "Covid PPE-KIT Mask Coverall" },
+          { "name": "twitter:card", "content": "summary_large_image" },
+          { "name": "twitter:site", "content": "@supplycovid" },
+          { "name": "twitter:creator", "content": "@supplycovid" },
+          { "name": "twitter_title", "content": "Buy Original PPE kits, n95 masks, nitrile gloves from SITRA certified Manufacturers | Covid Supply" },
+          { "name": "twitter_description", "content": "Buy Original PPE kits, n95 masks, nitrile gloves from SITRA certified Manufacturers | Covid Supply" },
           { property: "og:type", content: "organization" },
           { property: "og:title", content: "Buy SITRA certified PPE kits, n95 masks| Covid Supply" },
           { property: "og:description", content: "Buy certified PPE kits, N95 masks, Nitrile gloves, Face shields in Delhi, Mumbai, Bangalore, Indore, Hyderabad at best prices." },
           { property: "og:url", content: "http://covidsupply.in/about-us" },
           { property: "og:image", content: "http://covidsupply.in/coronavirus.png" }
         ]}
-       />
+      />
 
       <ExamplesNavbar />
       {/* <ProfilePageHeader /> */}
       <div className="m-t-100">
       </div>
-        <div className="row" style={{margin:'auto',marginLeft:'20px'}}>
-          <div className="col-sm-8">
-      <Tabs>
-    <TabList>
-      <Tab>All</Tab>
-      <Tab>Quote Related</Tab>
-      <Tab>Payment Related</Tab>
-      <Tab>Drawing related</Tab>
-      <Tab>Packing Lists</Tab>
-    </TabList>
- 
-    <TabPanel>
-      <div className="tab-body">
-      <h2>Any content 1</h2>
-      </div>
-    </TabPanel>
-    <TabPanel>
-    <div className="tab-body">
-      <h2>Any content 2</h2>
-      </div>
-    </TabPanel>
-    <TabPanel>
-    <div className="tab-body">
-      <h2>Any content 3</h2>
-      </div>
-    </TabPanel>
-    <TabPanel>
-    <div className="tab-body">
-      <h2>Any content 4</h2>
-      </div>
-    </TabPanel>
-    <TabPanel>
-    <div className="tab-body">
-      <h2>Any content 5</h2>
-      </div>
-    </TabPanel>
-  </Tabs>
-      </div>
-      {/* right section  */}
-      <div className="col-sm-3 m-t-35 m-l-50">
-        {/* rectangle 1 */}
+      <div className="row" style={{ margin: 'auto', marginLeft: '20px' }}>
+        <div className="col-sm-8">
+          <Tabs>
+            <TabList>
+              <Tab>All</Tab>
+              <Tab>Quote Related</Tab>
+              <Tab>Payment Related</Tab>
+              <Tab>Drawing related</Tab>
+              <Tab>Packing Lists</Tab>
+            </TabList>
 
-      <div className="rectangle">
-        <div style={{margin:'16px'}}>
-          <img src={RupeeIcon} className="rect-icon" style={{width:'20px'}} alt="rupee-icon"></img>
+            <TabPanel>
+              <div className="tab-body">
+                <h2>Any content 1</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-body">
+                <h2>Any content 2</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-body">
+                <h2>Any content 3</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-body">
+                <h2>Any content 4</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="tab-body">
+                <h2>Any content 5</h2>
+              </div>
+            </TabPanel>
+          </Tabs>
         </div>
-        <div className="rect-body">
-          Payment Summary
-        </div>
-        <div style={{margin:'16px'}}>
-          <img src={RightIcon} style={{width:'15px'}} alt="right-icon"></img>
-        </div>
+        {/* right section  */}
+        <div className="col-sm-3 m-t-35 m-l-50">
+          {/* rectangle 1 */}
 
-      </div>
-      {/* rectangle 2 */}
-      <Link to="/dashboard-tab">
-      <div className="rectangle">
-        <div style={{margin:'16px'}}>
-          <img src={RupeeIcon} className="rect-icon" style={{width:'20px'}} alt="rupee-icon"></img>
+          <div className="rectangle">
+            <div style={{ margin: '16px' }}>
+              <img src={RupeeIcon} className="rect-icon" style={{ width: '20px' }} alt="rupee-icon"></img>
+            </div>
+            <div className="rect-body">
+              Payment Summary
         </div>
-        
-        <div className="rect-body">
-          
-          Payment Summary
-        </div>
-       
-        <div style={{margin:'16px'}}>
-          <img src={RightIcon} style={{width:'15px'}} alt="right-icon"></img>
+            <div style={{ margin: '16px' }}>
+              <img src={RightIcon} style={{ width: '15px' }} alt="right-icon"></img>
+            </div>
+
+          </div>
+          {/* rectangle 2 */}
+          <Link to="/dashboard-tab">
+            <div className="rectangle">
+              <div style={{ margin: '16px' }}>
+                <img src={RupeeIcon} className="rect-icon" style={{ width: '20px' }} alt="rupee-icon"></img>
+              </div>
+
+              <div className="rect-body">
+
+                Payment Summary
         </div>
 
+              <div style={{ margin: '16px' }}>
+                <img src={RightIcon} style={{ width: '15px' }} alt="right-icon"></img>
+              </div>
+
+            </div>
+          </Link>
+
+          {/* rectangle 3 */}
+
+          <div className="rectangle">
+            <div style={{ margin: '16px' }}>
+              <img src={RupeeIcon} className="rect-icon" style={{ width: '20px' }} alt="rupee-icon"></img>
+            </div>
+            <div className="rect-body">
+              Payment Summary
+        </div>
+            <div style={{ margin: '16px' }}>
+              <img src={RightIcon} style={{ width: '15px' }} alt="right-icon"></img>
+            </div>
+
+          </div>
+
+          {/* rectangle 4 */}
+          <div className="rectangle">
+            <div style={{ margin: '16px' }}>
+              <img src={RupeeIcon} className="rect-icon" style={{ width: '20px' }} alt="rupee-icon"></img>
+            </div>
+            <div className="rect-body">
+              Payment Summary
+        </div>
+            <div style={{ margin: '16px' }}>
+              <img src={RightIcon} style={{ width: '15px' }} alt="right-icon"></img>
+            </div>
+
+          </div>
+
+        </div>
       </div>
-      </Link>
 
-      {/* rectangle 3 */}
 
-      <div className="rectangle">
-        <div style={{margin:'16px'}}>
-          <img src={RupeeIcon} className="rect-icon" style={{width:'20px'}} alt="rupee-icon"></img>
-        </div>
-        <div className="rect-body">
-          Payment Summary
-        </div>
-        <div style={{margin:'16px'}}>
-          <img src={RightIcon} style={{width:'15px'}} alt="right-icon"></img>
-        </div>
 
-      </div>
 
-      {/* rectangle 4 */}
-      <div className="rectangle">
-        <div style={{margin:'16px'}}>
-          <img src={RupeeIcon} className="rect-icon" style={{width:'20px'}} alt="rupee-icon"></img>
-        </div>
-        <div className="rect-body">
-          Payment Summary
-        </div>
-        <div style={{margin:'16px'}}>
-          <img src={RightIcon} style={{width:'15px'}} alt="right-icon"></img>
-        </div>
-
-      </div>
-
-      </div>
-      </div>
-    
-      
-  
-      
       <DemoFooter />
-      
+
     </>
   );
 }
